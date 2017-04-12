e9edfcb8d99d2d52d02c1eb708a3ea4383894979
Carlo Renzo Huamán Torres <tohure.gec@gmail.com>
2017-04-12 17:52:35 -0500

    update

---
+++ database/migrations/2017_04_12_223820_create_jobs_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title',100);
+            $table->string('description',200);
+            $table->string('company',100);
+            $table->date('date');
+            $table->timestamp('added_on');
+            $table->string('place',100);
+            $table->string('keywords',300);
+            $table->string('salary',100);
+            $table->enum('status', ['0', '1']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+}
