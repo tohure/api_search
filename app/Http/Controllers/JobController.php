caf95a3dd4b2ce2095928fbd4d326ee03130ce40
Carlo Renzo Huamán Torres <tohure.gec@gmail.com>
2017-04-17 00:43:57 -0500

    changes api

---
+++ app/Http/Controllers/JobController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Job;
+
+class JobController extends Controller
+{
+	public function index() {
+		$jobs = Job::where('status',"1")->get();
+
+		if ($jobs) {
+			return ['error' => 0, 'data' => $jobs];
+		}else{
+			return ['error' => 1, 'message' => "Bad Request"];
+		}
+
+	}
+}
